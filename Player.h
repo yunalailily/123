e96d91f2c763984599430690caa2d65c5f367cd6
joyce9233 <101238789+joyce9233@users.noreply.github.com>
2023-05-15 15:53:11 +0800

    player.h
    增加細項

--- Player.h
+++ Player.h
@@ -1,9 +1,17 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include<string>
+using namespace std;
 
 class Player
 {
-
+private:
+	int pokerF[5]; //玩家牌
+	int pokerNumF; //玩家牌數
+public:
+	string getPokerF(); //顯示玩家牌
+	int getSumF(); //判斷玩家是否>21
+	void farmerAsk(); //玩家要牌
+	void landlordProcess();
 };
 #endif 
-
