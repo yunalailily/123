bda1cff28d192399c8d5c6772b69827f03fb72ca
joyce9233 <101238789+joyce9233@users.noreply.github.com>
2023-05-15 15:56:22 +0800

    Dealer.h
    增加系項

--- Dealer.h
+++ Dealer.h
@@ -1,9 +1,16 @@
 #ifndef DEALER_H
 #define DEALER_H
+#include<string>
+using namespace std;
 
 class Dealer
 {
-
+private:
+	int pokerL[5]; //莊家牌
+	int pokerNumL; //莊家牌數
+public:
+	string getPokerL(); //顯示莊家牌
+	int getSumL(); //判斷莊家是否>=17
 };
 #endif 
 
