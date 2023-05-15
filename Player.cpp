5fef2a4483b318ce02e671cdb92a81a65d508290
joyce9233 <101238789+joyce9233@users.noreply.github.com>
2023-05-15 15:55:24 +0800

    Player.cpp
    增加細項

--- Player.cpp
+++ Player.cpp
@@ -1,7 +1,39 @@
-#include "Player.h"
-#include <iostream>
-#include <cstdlib> 
-#include <ctime> 
+#include"Player.h"
+#include<string>
+#include<iostream>
 using namespace std;
-Player::Player() {
+
+
+string Player::getPokerF() //顯示玩家牌
+{
+	int i;
+	string result = " ";
+	for (int i = 0; i < pokerNumF; i++)
+		result += pokerF[i] + " ";
+	return result;
+}
+int Player::getSumF() //判斷玩家是否>21
+{
+	int result = 0;
+	for (int i = 0; i < pokerNumF; i++)
+		result += pokerF[i];
+	return result;
+}
+void Player::farmerAsk() //玩家要牌
+{
+	if (pokerNumF >= 5)
+	{
+		cout << "無法要牌";
+	}
+	else
+	{
+		if (getSumF() > 21)
+		{
+			cout << "BUST";
+		}
+		else if (getSumF() == 21)
+		{
+			landlordProcess();
+		}
+	}
 }
