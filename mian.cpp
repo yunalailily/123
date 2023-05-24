2d8f1a79a206a7e424e080215b4c5fd36009f05c
joyce9233 <101238789+joyce9233@users.noreply.github.com>
2023-05-24 19:08:24 +0800

    Update mian.cpp
    main

--- mian.cpp
+++ mian.cpp
@@ -1,12 +1,23 @@
-#include "Dealer.h"
-#include "Player.h"
 #include <iostream>
+#include "Poker.h"
 using namespace std;
-int main() {
-	cout << "歡迎來到21點遊戲~" << endl;
-	Dealer computer;
-	Player p;
-	cout << "遊戲開始!" << endl;
-	computer.deal();
-	computer.getMoreCard();
+
+int main()
+{
+    Poker p;
+    int game = 1;
+    p.deal();
+    cout << "歡迎來到21點" << endl;
+    while (game == 1 || game == 2)
+    {
+        cout << "1.要牌 2.不要牌 3.退出  >>請輸入數字選擇操作:";
+        cin >> game;
+        if (game == 1) {
+            p.checkP();
+        }
+        else if (game == 2) {
+            p.check();
+        }
+        else      exit(0);
+    }
 }
