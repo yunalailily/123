f3391f0fdf13bd747d2a9653de455fe6141e9c77
yunalailily <101238577+yunalailily@users.noreply.github.com>
2023-05-25 14:24:51 +0800

    Update mian.cpp

--- mian.cpp
+++ mian.cpp
@@ -5,19 +5,20 @@ using namespace std;
 int main()
 {
     Poker p;
-    int game = 1;
+    char card = 'Y';
     p.deal();
-    cout << "歡迎來到21點" << endl;
-    while (game == 1 || game == 2)
+    cout << "Welcome to game-POKERFACE" << endl;
+    while (card == 'Y' || card == 'N')
     {
-        cout << "1.要牌 2.不要牌 3.退出  >>請輸入數字選擇操作:";
-        cin >> game;
-        if (game == 1) {
+        cout << "Ask for a card? Y/N/Q";
+        cin >> card;
+        if (card == 'Y') {
             p.checkP();
         }
-        else if (game == 2) {
+        else if (card == 'N') {
             p.check();
         }
-        else      exit(0);
+        else      
+            return 0;
     }
 }
