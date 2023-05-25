dc03162a3d93265e89e234d51b794e82831cf7dc
yunalailily <101238577+yunalailily@users.noreply.github.com>
2023-05-25 14:25:31 +0800

    Update Poker.h

--- Poker.h
+++ Poker.h
@@ -8,20 +8,20 @@ private:
 	int poker[52];
 	int pValue[52];
 	string pName[52];
-	int pokerP[5]; //玩家牌
-	int pokerD[5]; //莊家牌(pokerB)
-	int pokerPNum; //玩家牌數
-	int pokerDNum; //莊家牌數(pokerBNum)
+	int pokerP[5]; 
+	int pokerD[5];
+	int pokerPNum; 
+	int pokerDNum; 
 
 public:
 	Poker();
-	void deal(); //發牌  (shuffle())
-	int getSumP(); //玩家牌加總
-	int getSumD(); //莊家牌加總(getSumB())
-	string getPokerP(); //顯示玩家牌
-	string getPokerD(); //顯示莊家牌(getPokerB())
-	void checkP(); //檢查玩家牌 (outs)
-	void checkD(); //檢查莊家牌 (Bouts)
-	void check(); //檢查 (BBouts)
+	void deal(); 
+	int getSumP();
+	int getSumD();
+	string getPokerP(); 
+	string getPokerD(); 
+	void checkP(); 
+	void checkD(); 
+	void check(); 
 };
 #endif
